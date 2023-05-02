43adc86a792ee878c810dda9228e2fa631adafd6
Kevin Quispe <kevincarlosqa@gmail.com>
2023-05-02 17:09:03 -0500

    add: Associations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  belongs_to :parent
+  belongs_to :replied_to, class_name: "Tweet", optional: true
+  has_many :likes, dependent: :destroy
+  has_many :users, through: :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :tweets, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  # has_many :tweets, through: :likes
 end
