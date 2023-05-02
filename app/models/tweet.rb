d6f09533c32c343a145cc9506ef471bbcbf29b87
Kevin Quispe <kevincarlosqa@gmail.com>
2023-05-02 17:36:28 -0500

    add: Validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,14 @@
 class Tweet < ApplicationRecord
+  # Associations
   belongs_to :user
   belongs_to :replied_to, class_name: "Tweet", optional: true
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
+  has_many :replies, class_name: "Tweet",
+                     foreign_key: "replied_to_id",
+                     dependent: :nullify,
+                     inverse_of: "replied_to"
+
+  # Validations
+  validates :body, presence: true, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # Associations
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
   # has_many :tweets, through: :likes
+
+  # Validations
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
+  enum :role, { member: 0, admin: 1 }
 end
